5e29cd1a3227e665031a5b4e79962a64b002c2b9
G-a-b-b-s <gaba.czapska@onet.pl>
2024-10-11 08:32:23 +0200

    Added go logic - fixed buggs

--- handling.py
+++ handling.py
@@ -57,7 +57,7 @@ class LanguageApp:
 
 
 
-    def display_noun_selection(self):
+    def display_noun_selection(self, arg=False):
         label = tk.Label(self.master, text="Select a noun:")
         label.pack(pady=10)
 
@@ -75,7 +75,7 @@ class LanguageApp:
         self.adjective_check.var = tk.BooleanVar()
         self.adjective_check["variable"] = self.adjective_check.var
         self.adjective_check.pack(pady=10)
-        self.select_button = tk.Button(self.master, text="Select", command=self.confirm_noun)
+        self.select_button = tk.Button(self.master, text="Select", command=lambda: self.confirm_noun(arg))
         self.select_button.pack(pady=10)
 
 
@@ -102,7 +102,7 @@ class LanguageApp:
 
 
 
-    def confirm_noun(self):
+    def confirm_noun(self, arg):
         selected_noun = self.noun_selection.get()
         selected_number = self.number_selection.get()
 
@@ -116,12 +116,22 @@ class LanguageApp:
             selected_noun = blob.pluralize()
 
         self.user_selection['subject_type'] = 'noun'
-        self.user_selection['subject' if 'subject' not in self.user_selection else 'complement'] = selected_noun
+
+        if not self.is_complement:
+            self.user_selection['subject'] = selected_noun
+        else:
+            if 'complement' not in self.user_selection:
+                self.user_selection['complement'] = {}
+
+            self.user_selection['complement']['subject'] = selected_noun
+
         if hasattr(self, 'adjective_selection'):
             selected_adjective = self.adjective_selection.get()
             if selected_adjective:
                 if not self.is_complement:
                     self.user_selection['subject'] = f"{selected_adjective} {selected_noun}"
+                if arg:
+                    self.user_selection['complement']['subject'] = f"{selected_adjective} {selected_noun}"
                 else:
                     self.user_selection['complement'] = f"{selected_adjective} {selected_noun}"
 
@@ -181,34 +191,37 @@ class LanguageApp:
     def display_verb_selection(self, go=False):
         self.clear_window()
         if go:
+            self.clear_window()
             label = tk.Label(self.master, text="Select a verb:")
             label.pack(pady=10)
             verbs.remove('Go')
             self.verb_as_complement_selection = ttk.Combobox(self.master, values=verbs)
             self.verb_as_complement_selection.pack(pady=10)
             self.confirm_button = tk.Button(self.master, text="Select", command=self.confirm_go_second_verb_selection)
+            self.confirm_button.pack(pady=10)
         # Display verb selection
-        label = tk.Label(self.master, text="Select a verb:")
-        label.pack(pady=10)
+        else:
+            label = tk.Label(self.master, text="Select a verb:")
+            label.pack(pady=10)
 
-        self.verb_selection = ttk.Combobox(self.master, values=verbs)
-        self.verb_selection.pack(pady=10)
+            self.verb_selection = ttk.Combobox(self.master, values=verbs)
+            self.verb_selection.pack(pady=10)
 
-        # Display mood selection
-        mood_label = tk.Label(self.master, text="Select mood:")
-        mood_label.pack(pady=10)
+            # Display mood selection
+            mood_label = tk.Label(self.master, text="Select mood:")
+            mood_label.pack(pady=10)
 
-        self.mood_selection = ttk.Combobox(self.master, values=moods)
-        self.mood_selection.pack(pady=10)
-        self.mood_selection.bind("<<ComboboxSelected>>", self.mood_selected)
+            self.mood_selection = ttk.Combobox(self.master, values=moods)
+            self.mood_selection.pack(pady=10)
+            self.mood_selection.bind("<<ComboboxSelected>>", self.mood_selected)
 
-        # Display tense selection (initially hidden)
-        self.tense_label = tk.Label(self.master, text="Select tense:")
-        self.tense_selection = ttk.Combobox(self.master, values=tenses)
+            # Display tense selection (initially hidden)
+            self.tense_label = tk.Label(self.master, text="Select tense:")
+            self.tense_selection = ttk.Combobox(self.master, values=tenses)
 
-        if self.mood_selection.get() == "Imperative":
-            if self.verb_selection.get().lower() in ["go", "be"]:
-                self.confirm_button = tk.Button(self.master, text="Select", command=self.go_or_be_confirm_verb_selection())
+            if self.mood_selection.get() == "Imperative":
+                if self.verb_selection.get().lower() in ["go", "be"]:
+                    self.confirm_button = tk.Button(self.master, text="Select", command=self.go_or_be_confirm_verb_selection())
             self.confirm_button = tk.Button(self.master, text="Select", command=self.confirm_verb_selection)
 
     def mood_selected(self, event):
@@ -269,6 +282,7 @@ class LanguageApp:
 
         verb = self.user_selection.get('verb', {}).get('verb', '').lower()
         mood = self.user_selection.get('verb', {}).get('mood', '').lower()
+
         func = self.add_complement
 
         if mood == 'imperative' and verb =='go' and go==False:
@@ -289,9 +303,13 @@ class LanguageApp:
 
     def add_complement(self):
 
+        verb = self.user_selection.get('verb', {}).get('verb', '')
+        arg=False
+        if verb:
+            arg =True
         self.is_complement = True
         self.clear_window()
-        self.display_noun_selection()
+        self.display_noun_selection(arg)
 
     def go_complement(self):
         self.clear_window()
@@ -313,7 +331,7 @@ class LanguageApp:
         verb = self.user_selection.get('verb', {}).get('verb', '')
         mood = self.user_selection.get('verb', {}).get('mood', '').lower()
         tense = self.user_selection.get('verb', {}).get('tense', '').lower()
-        complement = self.user_selection.get('complement', '')
+        complement = self.user_selection.get('complement', {})
         number = self.user_selection.get('noun_number', '')
 
         # Verb conjugation
@@ -333,11 +351,13 @@ class LanguageApp:
             # Building the final sentence
             final_sentence = f"{subject.capitalize()} {verb_conjugated}"
             if complement:
-                final_sentence += f" {complement}"
+                final_sentence += f" {complement['subject']}"
         else:
             final_sentence = f"{verb.capitalize()}"
             if complement:
-                final_sentence += f" {complement}"
+                verb_comp = complement.get('verb','')
+                subject_comp= complement.get('subject','')
+                final_sentence += f" {verb_comp.lower()} {subject_comp.lower()}"
 
         # Displaying the final sentence
         label = tk.Label(self.master, text=f"Final sentence: {final_sentence}")
